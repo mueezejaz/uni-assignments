a6ce8050a340295b610ff7672ef4493583833877
abdul mueez <mueezejaz12345678@gmail.com>
2024-10-05 14:39:01 +1200

    set up client

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv  from "dotenv";
+dotenv.config()
+const port = process.env.PORT||3000
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/test", (req, res) => {
+    res.status(200).json({
+      sucess: true,
+      trst: "apis are running",
+    });
+  });
+app.listen(port,()=>{
+    console.log("server is running on port ",port)
+})
